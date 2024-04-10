d9088e9afd53dc2ef96ce80aecfad303702a00f7
kurodenjiro <kurodenjiro1@gmail.com>
2024-04-11 05:44:26 +0700

    add

---
+++ data/bos/js/button-style.js
@@ -0,0 +1,15 @@
+const style = {
+    padding: "1rem 2rem",
+    borderRadius: "8px",
+    backgroundColor: "#e74c3c",
+    color: "#fff",
+    fontSize: "1.2rem",
+    border: "none",
+    cursor: "pointer"
+  };
+  
+  return (
+    <>
+      <button style={style}>Click Me!</button>
+    </>
+  );
